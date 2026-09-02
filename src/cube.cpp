7c1f48676390591889b7398fb73753110a0b0ffa
cisdeli <pedrohcisdeli@gmail.com>
2026-09-01 20:10:39 -0400

    chore: removing stale code from the wrong file

--- src/cube.cpp
+++ src/cube.cpp
@@ -1,11 +1,1 @@
 #include "cube.h"
-#include <vector>
-
-#ifdef __CUDACC__
-  #define CUBE_HD __host__ __device__
-#else
-  #define CUBE_HD
-#endif
-
-/* CUBE_HD inline Cube applyMove(Cube c, Move m) { ... } */
-
